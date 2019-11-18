884ed427c4c606c9f81a9272166afbf65351f523
Jerome Petazzoni <jerome.petazzoni@gmail.com>
2019-11-18 06:19:01 -0600

    Add helper script

---
+++ shpod.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+if [ -f shpod.yaml ]; then
+  YAML=shpod.yaml
+else
+  YAML=https://raw.githubusercontent.com/jpetazzo/shpod/master/shpod.yaml
+fi
+echo "Applying YAML: $YAML..."
+kubectl apply -f $YAML
+echo "Waiting for pod to be ready..."
+kubectl wait --namespace=shpod --for condition=Ready pod/shpod
+echo "Attaching to the pod..."
+kubectl attach --namespace=shpod -ti shpod
+echo "Deleting pod..."
+echo "
+Note: it's OK to press Ctrl-C if this takes too long and you're impatient.
+Clean up will continue in the background. However, if you want to restart
+shpod, you need to wait a bit (about 30 seconds).
+"
+kubectl delete -f $YAML --now
+echo "Done."
